5459adf5ce57336be757d56f381c38213eb85b5e
roberto-mazor <mazor.estudo@gmail.com>
2025-12-22 19:22:26 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -6,10 +6,9 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false // Essencial para Neon na Vercel
     }
-  },
-  logging: false
+  }
 });
 
 const testConnection = async () => {
