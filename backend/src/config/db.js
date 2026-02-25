e20b5f7818a8825f0d106387acd04ac2077c2d7e
Roberto Mazor <mazor.estudo@gmail.com>
2026-02-24 22:11:50 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,5 +1,6 @@
 // src/config/db.js
-const { Sequelize } = require('sequelize'); // Única declaração permitida
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
